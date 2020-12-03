a9fac312514997b22542736cc698e87b55c64ced
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-12-03 15:54:22 +0100

    lijkt nu wel te werken

--- 07-lit-element/exercise-01/to-do-item.js
+++ 07-lit-element/exercise-01/to-do-item.js
@@ -1,8 +1,12 @@
-import { LitElement, html } from 'lit-element';
+import { css,LitElement, html } from 'lit-element';
 
 export class TodoItem extends LitElement {
 	static get properties() {
-		return { text: { type: String },checked: {type:Boolean} };
+		return {
+			text: { type: String },
+			checked: {type:Boolean},
+			index:{type:Number}
+		};
 	}
 	constructor() {
 		super();
@@ -10,23 +14,25 @@ export class TodoItem extends LitElement {
 		this.checked=false;
 		this.index=0;
 	}
+	static get styles() {
+		// language=css
+		return css`.completed {
+				text-decoration: line-through;
+			}
+			:host {
+				display: block;
+				font-family: sans-serif;
+			}`
+	}
 	render() {
 		return html`
-			<li class="item">
+			<li class="item ${this.checked?'completed':''}" >
 				<input type="checkbox" ?checked="${this.checked}" @click="${this._clickHandler}">
 				<label>${this.text}</label>
-			</li>
-			<style>
-				.completed {
-					text-decoration: line-through;
-				}
-				:host {
-					display: block;
-					font-family: sans-serif;
-				  }
-			</style>`
+			</li>`
 	}
 	_clickHandler() {
+		this.checked=!this.checked;
 		this.dispatchEvent(new CustomEvent('onToggle', {
 			detail: this.getAttribute('index'),
 			composed: true,
--- 07-lit-element/exercise-01/to-do-list.js
+++ 07-lit-element/exercise-01/to-do-list.js
@@ -1,17 +1,16 @@
-import { LitElement, html } from 'lit-element';
+import { css, LitElement, html } from 'lit-element';
 
 import './to-do-item.js';
 
 export class ToDoList extends LitElement {
-	render() {
-		return html`
-			<h1>To do</h1>
-			<input type="text" placeholder="Add a new to do"></input>
-			<button @click="${this._handleClick}">Voeg toe</button>
-		
-			<ul id="todos"></ul>
-			<style>
-			  :host {
+	static get properties() {
+		return {
+			todos:{type:Array}
+		};
+	}
+	static get styles() {
+		// language=css
+		return css`:host {
 				display: block;
 				font-family: sans-serif;
 			  }
@@ -36,60 +35,29 @@ export class ToDoList extends LitElement {
 				cursor: pointer;
 				font-size: 1em;
 				padding: 0.5em 1em;
-			  }
-			</style>
-
-		`
+			  }`
 	}
-
-	_toggleTodo(event) {
-		// Find the to do that fired the event using event.detail as its index
-		const todo = this._todos[event.detail];
-
-		todo.checked = !todo.checked;
-		this._renderTodoList();
+	constructor() {
+		super();
+		this.todos=[];
+	}
+	render() {
+		return html`
+			<h1>To do</h1>
+			<input type="text" placeholder="Add a new to do">
+			<button @click="${this._handleClick}">Voeg toe</button>
+		
+			<ul id="todos">${this.todos.map(todo=>html`<to-do-item text="${todo.text}" ?checked="${todo.checked}" index="${todo.index}"></to-do-item>`)}</ul>`
 	}
 
 	_handleClick() {
 		if (this.shadowRoot.querySelector('input').value.length > 0) {
-			this._todos.push({
+			this.todos.push({
 				text: this.shadowRoot.querySelector('input').value,
 				checked: false
 			})
-			this._renderTodoList();
-			this.shadowRoot.querySelector('input').value = '';
+			this.todos=[...this.todos]
 		}
-	}
-
-	_renderTodoList() {
-		this.shadowRoot.querySelector('#todos').innerHTML = '';
-
-		this._todos.forEach((todo, index) => {
-			let $todoItem = document.createElement('to-do-item');
-			$todoItem.setAttribute('text', todo.text);
-
-			// if our to-do is checked, set the attribute, else; omit it.
-			if (todo.checked) {
-				$todoItem.setAttribute('checked', '');
-			}
-
-			// By setting index we have some state to keep track of the index
-			// of the to do
-			$todoItem.setAttribute('index', index);
-			$todoItem.addEventListener('onToggle', this._toggleTodo.bind(this));
-			this.shadowRoot.querySelector('#todos').appendChild($todoItem);
-		});
-	}
-
-	// Implement getters and setters to be able to pass data to the component
-	// and render a list of to do's.
-	set todos(value) {
-		this._todos = value;
-		this._renderTodoList();
-	}
-
-	get todos() {
-		return this._todos;
 	}
 }
 customElements.define('to-do-list', ToDoList)
